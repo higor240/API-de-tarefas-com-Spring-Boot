30b067d8356ba4bce3d73b4964481c35c81ab9bc
Higor <higorferreira240@gmail.com>
2025-10-23 01:34:25 -0300

    configuração da entidade ToDo

---
+++ src/main/java/com/crud/crud/entity/ToDo.java
@@ -0,0 +1,53 @@
+package com.crud.crud.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ind;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+    
+    public long getInd() {
+        return ind;
+    }
+    public void setInd(long ind) {
+        this.ind = ind;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public boolean isRealizado() {
+        return realizado;
+    }
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    
+}
